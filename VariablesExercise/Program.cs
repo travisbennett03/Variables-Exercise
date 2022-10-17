dd55cf86aaec6fe506f72d60a3191ca6c52076f8
travisbennett03 <tbennett1996@yahoo.com>
2022-10-17 13:50:46 -0500

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Variable Declerations
+            int age;
+            string name;
+            bool isAlive;
+            char namestartswith;
+            double moneyspentoncoffee;
+            decimal height; 
+            
+            // Variable Initalizations
+            age = 26;
+            name = "Travis";
+            isAlive = true;
+            namestartswith = 'T';
+            moneyspentoncoffee = 6.42;
+            height = 6;
+
+
+            Console.WriteLine($"My name is {name}. I am {age}. is he alive: {isAlive}. My name starts with a {namestartswith}. Coffee this morning was {moneyspentoncoffee}. I am {height} feet tall");
         }
     }
 }
